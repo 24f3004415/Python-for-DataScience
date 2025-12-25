57283903b3c126a2ebb800c54c2cd4cfb32c146c
Mohit Rai <mohiit.rai.007@gmail.com>
2025-12-25 19:16:07 +0530

    Factors

---
+++ Question Practice/Basic Maths/5-Factors.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+num = int(input('Enter a number: '))
+factors = []
+for i in range(1,int(sqrt(num))+1):
+    if num%i == 0:
+        factors.append(i)
+        if num//i != i:
+            factors.append(num//i)
+
+print(factors)
