dada5a34e1dc19079001837844058d06427f1bf8
Mohit Rai <mohiit.rai.007@gmail.com>
2026-01-11 20:48:40 +0530

    Selection SOrt done

---
+++ DSA SHEET QUESTIONS/SORTING/Selection_Sort.py
@@ -0,0 +1,14 @@
+def SelectionSort(L):
+    n = len(L)
+    if n<=1:
+        return L
+    
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1,n):
+            if L[j] > L[min_idx]:
+                min_idx = j
+        L[i],L[min_idx] = L[min_idx],L[i]
+    return L
+
+print(SelectionSort([4,5,2,1,3]))
