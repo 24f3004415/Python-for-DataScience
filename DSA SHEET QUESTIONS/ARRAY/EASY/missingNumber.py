1ac09d055c8e6f7485972c0cd2c95a67f7389a95
Mohit Rai <mohiit.rai.007@gmail.com>
2026-01-27 21:59:49 +0530

    OOPS classes and objects

---
+++ DSA SHEET QUESTIONS/ARRAY/EASY/missingNumber.py
@@ -0,0 +1,4 @@
+nums = [9,6,4,2,3,5,7,0,1]
+n = len(nums)
+
+print(sum(num for num in range(0,n+1)) - sum(nums))
---
+++ OOPS/classes&objects.py
@@ -0,0 +1,31 @@
+# class SharmaVishnu:
+#     a = 10
+
+#     def show():
+#         print('chole bhature')
+
+# SharmaVishnu.show()
+# print(SharmaVishnu.a)
+
+
+
+
+class SharmaVishnu:
+
+    a = 10
+
+    def show(self):
+        print('Biryani')
+
+MP_Nagar = SharmaVishnu()
+MP_Nagar.show()
+
+NewMarket = SharmaVishnu()
+NewMarket.show()
+
+class SagarGaire:
+    def show2(self):
+        print('Cheese sandwich')
+
+patel_nagar = SagarGaire()
+patel_nagar.show2()
