270e6a80af7b09096b6c38e3693c100f01545354
Mohit Rai <mohiit.rai.007@gmail.com>
2026-02-05 20:44:07 +0530

    OOPS-Inheritance

---
+++ OOPS/Inheritance.py
@@ -0,0 +1,36 @@
+# single level inheritance
+#  Multiple level inheritance
+# multilevel
+
+class Animal:
+    def __init__(self, name,age,):
+        self.name = name
+        self.age = age
+        self.canWalk = True
+        self.__gender = "Male"
+
+
+    def Show(self):
+        print(f"Name of the dog is {self.name} and it's age is {self.age}")
+
+    def Sleep(self):
+        print(f"Do cats Sleep? --> {self.canWalk}")
+
+    def gender(self):
+        print(f"Do cats Sleep? --> {self.__gender}")
+
+class Cat(Animal):
+    def __init__(self, name, age, breed):
+        super().__init__(name, age)
+        self.breed = breed
+
+    def Show2(self):
+        print("This is cat class!! Inherited Animal class")
+        print(f"{self.name} and {self.age} and breed is {self.breed} and gender is {self.__gender}")
+        print("Hence Proved")
+
+obj = Cat("Shera", 4, "bagadbilla")
+obj.Show2()
+obj.Show()
+obj.gender()
+obj.Sleep()
