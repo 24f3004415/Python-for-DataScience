761d7da5a7ee072c2bfa4758beab42433d234afe
Mohit Rai <mohiit.rai.007@gmail.com>
2026-02-12 17:25:35 +0530

    demonstrate instance methods within a `Laptop` class.

--- OOPS/MethodTypes/01_Instance Methods.py
+++ OOPS/MethodTypes/01_Instance Methods.py
@@ -2,15 +2,17 @@
 # • Take self as the first argument.
 # • Can access both instance attributes and class attributes.
 
-class laptop:
-    storage_type = "SSD"
 
-    def __init__(self, Ram, Storage):
+class Laptop:
+    storage_type = "SSD"   # ---> Storage_type is Class Attribute.
+
+    def __init__(self, Ram, Storage):    #---> Self.Ram & Self.Storage are Instance Attributes
         self.Ram = Ram
         self.Storage = Storage
 
-    def get_info(self):     #Instance Methdos
+    def get_info(self):     # ---> get_info is Instance Method because it is taking self as 1st argument.
         print(f"The Laptop has {self.Ram} GB of RAM, {self.Storage} GB of {self.storage_type}")
 
-laptop1 = laptop(16,512)
+
+laptop1 = Laptop(16,512)
 laptop1.get_info()
