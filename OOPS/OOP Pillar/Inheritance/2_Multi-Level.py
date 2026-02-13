b929f398f5d214caf56f909b5a8e102c933a858a
Mohit Rai <mohiit.rai.007@gmail.com>
2026-02-14 01:59:58 +0530

    Types of Inheritance

---
+++ OOPS/OOP Pillar/Inheritance/2_Multi-Level.py
@@ -0,0 +1,13 @@
+class Animal:
+    pass
+
+class Mammal(Animal):
+    pass
+
+class Dog(Mammal):
+    pass
+
+
+# Dog → Mammal → Animal
+
+# In multilevel inheritance, a class inherits from a class which itself inherits from another class.
---
+++ OOPS/OOP Pillar/Inheritance/3_Multiple.py
@@ -0,0 +1,13 @@
+class Father:
+    pass
+
+class Mother:
+    pass
+
+class Child(Father, Mother):
+    pass
+
+
+# Child → Father + Mother
+
+# In multiple inheritance, a subclass inherits from more than one superclass.
