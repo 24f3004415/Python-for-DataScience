06ad05343605324cd2652b852f7ad46e78ec313b
Mohit Rai <mohiit.rai.007@gmail.com>
2026-02-14 21:14:12 +0530

    Initial banking system with data persistence and account creation functionality.

--- OOPS/Project/BANKING/main.py
+++ OOPS/Project/BANKING/main.py
@@ -17,5 +17,32 @@ class Bank:
     except Exception as err:
         print(f"Error occurred: {err}")
 
+    @classmethod
+    def update(cls):
+        with open(Bank.database, 'w') as fs:
+            fs.write(json.dumps(cls.data))
+
+    # create user
+    def createAccount(self):
+        info = {
+            'name' : input("Enter your name: "),
+            'age' : int(input("Enter your age: ")),
+            'email' : input("ENter your email: "),
+            'pin' : int(input("Enter your pin: ")),
+            'phone No.' : int(input("ENter your phone No.")),
+            'account No.' : 1234567890,
+            'balance' : 0
+        }
+
+        if info['age'] > 18 and len(str(info['pin'])) == 4 and len(str(info['phone No.'])) == 10:
+            Bank.data.append(info)
+            Bank.update()
+            print('Data added in a list.')
+
+        else:
+            print("Account cannot be created due to Wrong Credentials !!!")
+            
+
 obj = Bank()
+obj.createAccount()
 
